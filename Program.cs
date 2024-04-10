2a07a239cac414b6e586f66127a1a4a6a5312aa9
kostenkovitalina <152108911+kostenkovitalina@users.noreply.github.com>
2024-04-10 14:41:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,36 +1,38 @@
-﻿using System;
+using System;
 using System.Diagnostics;
+using static System.Net.WebRequestMethods;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Виберіть гілку для виклику:");
+        Console.WriteLine("Виберіть гілку для запуску (Vitalina або Kristina):");
         string branchName = Console.ReadLine();
 
-        CheckoutBranch(branchName); // Перехід на вказану гілку
-
-        RunCodeFromBranch(branchName); // Запуск коду з вказаної гілки
+        switch (branchName)
+        {
+            case "Vitalina":
+                OpenAndRunGitHubFile("Vitalina/start-programs.cs");
+                break;
+            case "Kristina":
+                OpenAndRunGitHubFile("Kristina/назва_файлу.cs");
+                break;
+            default:
+                Console.WriteLine("Невідома гілка");
+                break;
+        }
     }
 
-    static void CheckoutBranch(string branchName)
+    static void OpenAndRunGitHubFile(string branchName)
     {
-        // Використовуйте git checkout для переходу на вказану гілку
-        ProcessStartInfo psi = new ProcessStartInfo();
-        psi.FileName = "git";
-        psi.Arguments = "checkout " + branchName;
-        Process.Start(psi).WaitForExit();
-    }
-
-    static void RunCodeFromBranch(string branchName)
-    {
-        // Перед запуском коду з гілки, переконайтеся, що ви перебуваєте на неї
+        // Відкриваємо файл та запускаємо його
         Process.Start(new ProcessStartInfo
         {
             FileName = "cmd",
-            Arguments = $"/c dotnet run",
-            WorkingDirectory = $"https://github.com/kostenkovitalina/l/blob/5cc24024675c934a7bf91bb7a59914eec3f1d8c1/start-programs.cs{branchName}" // Замініть шлях на реальний шлях до вашого проекту
-        }).WaitForExit();
+            Arguments = $"/c start {branchName}/start-programs.cs & dotnet run",
+            UseShellExecute = true,
+            WorkingDirectory = @"https://github.com/kostenkovitalina/l.git"
+        });
     }
 }
 
